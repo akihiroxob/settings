f7ed5e6e31deaacadb6ca9c8aef3f8e262902163
Akihiro OKAYAMA <akihiroxob@yahoo.co.jp>
2017-10-26 20:09:20 +0900

    add textlint

---
+++ eslint/setup.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+npm install -g eslint@latest eslint-plugin-react@latest babel-eslint@latest
---
+++ textlint/setup.sh
@@ -0,0 +1,7 @@
+#! /bin/sh
+
+npm install -g textlint@latest \
+    textlint-rule-ginger@latest \
+    textlint-rule-spellchecker@2.1.0 \
+    textlint-rule-write-good@latest
+
